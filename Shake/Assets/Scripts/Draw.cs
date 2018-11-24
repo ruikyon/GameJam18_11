f4bcc9d4c21948b36168bf1a7478e6032b5ba2cd
DESKTOP-MUA8S6O\rui <meranazunx@gmail.com>
2018-11-24 11:48:08 +0900

    開始

---
+++ Shake/Assets/Scripts/Draw.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour {
+    [SerializeField]
+    private GameObject point_prefab;
+    UdpReceiver receiver;
+    private int size = 31;
+    private GameObject[] points;
+    private List<float> positions = new List<float>();
+
+	// Use this for initialization
+	void Start () {
+        points = new GameObject[size];
+        for(int i=0;i<size;i++)
+        {
+            var temp = Instantiate(point_prefab, new Vector3(-7.5f + (float)15/(size-1)*i, 0), new Quaternion(0, 0, 0, 0));
+            points[i] = temp;
+        }
+        receiver = new UdpReceiver(AccelAction);
+        receiver.UdpStart();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (positions.Count == size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                points[i].transform.position = new Vector3(points[i].transform.position.x, positions[i]);
+            }
+        }		
+	}
+
+    public void AccelAction(float x, float y, float z)
+    {
+        positions.Add(y*0.5f);
+        if(positions.Count > size)positions.RemoveAt(0);
+        if (x > y && x > z && x>1) Debug.Log("max: x");
+        else if (y > z && y>1) Debug.Log("max: y");
+        else if(z>1) Debug.Log("max: z");
+    }
+}
---
+++ Shake/Assets/Scripts/UdpReceiver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+
+public class UdpReceiver
+{
+    private const int LOCAL_PORT = 22222;
+    static UdpClient udp;
+    Thread thread;
+
+    private static Action<float, float, float> AccelCallBack;
+
+    public UdpReceiver(Action<float, float, float> action)
+    {
+        AccelCallBack += action;
+    }
+
+    public void UdpStart()
+    {
+        udp = new UdpClient(LOCAL_PORT);
+        udp.Client.ReceiveTimeout = 3000;
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    private static void ThreadMethod()
+    {
+        while (true)
+        {
+            byte[] data;
+            try
+            {
+                IPEndPoint remoteEp = null;
+                data = udp.Receive(ref remoteEp);
+
+                string text = Encoding.ASCII.GetString(data);
+
+                JsonNode jsonNode = JsonNode.Parse(text);
+
+                double ax = jsonNode["sensordata"]["accel"]["x"].Get<double>();
+                double ay = jsonNode["sensordata"]["accel"]["y"].Get<double>();
+                double az = jsonNode["sensordata"]["accel"]["z"].Get<double>();
+
+                AccelCallBack((float)ax, (float)ay, (float)az);
+                //Debug.Log("(x, y, z): "+ax);
+            }
+            catch (SocketException se)
+            {
+                udp.Close();
+                udp = new UdpClient(LOCAL_PORT);
+                udp.Client.ReceiveTimeout = 3000;
+                Debug.Log(se);
+            }
+            catch (NullReferenceException nre)
+            {
+                Debug.Log(nre);
+            }
+            catch (InvalidCastException ice)
+            {
+                Debug.Log(ice);
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        thread.Abort();
+        udp.Close();
+    }
+}
